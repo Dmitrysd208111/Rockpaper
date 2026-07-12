b102bf04b71b6b41935a95549bee9b085673ccd8
Dmitri <tsukiflow132@gmail.com>
2026-07-12 19:40:38 +0300

    rewrite js

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
-console.log("Hello world")
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice(){
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random()*3);
+    return choices[randomIndex];
+}
+
+function getHumanChoice() {
+    const humanChoice = prompt("Напиши rock, paper или scissors");
+    return humanChoice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice){
+switch(humanChoice){
+    case "rock":
+        if (computerChoice === "rock") {
+            console.log("Draw!");}
+        else if (computerChoice === "paper")
+            {
+            console.log ("ComPuter won!");
+            computerScore++;
+        }
+        else{
+            console.log("Human won!");
+            humanScore++;
+        }
+break;
+
+    case "paper":
+        if (computerChoice === "paper") {
+            console.log("Draw!");}
+        else if (computerChoice === "scissors")
+            {
+            console.log ("ComPuter won!");
+            computerScore++;
+        }
+        else{
+            console.log("Human won!");
+            humanScore++;
+        }
+        break;
+        
+    
+    case "scissors":
+        if (computerChoice === "scissors") {
+            console.log("Draw!");}
+        else if (computerChoice === "rock")
+            {
+            console.log ("ComPuter won!");
+            computerScore++;
+        }
+        else{
+            console.log("Human won!");
+            humanScore++;
+        }
+        break;
+    
+}
+}
+
+playRound(getHumanChoice(), getComputerChoice());
+
+console.log("Human:", humanScore);
+console.log("Computer:", computerScore);
